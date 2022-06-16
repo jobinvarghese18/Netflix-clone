cdf93c0361b4f07176c235eee8361fd700d3ad98
Jobin T V <jobinvarghese18@gmail.com>
2022-06-16 19:57:45 +0530

    feat: Banner

--- components/2-molecules/Banner/Banner.tsx
+++ components/2-molecules/Banner/Banner.tsx
@@ -24,7 +24,7 @@ export const Banner = (props: Props) => {
                     objectFit="cover" loader={null} />
             </div>
 
-            <h1 className="text-2xl font-bold md:text-4xl lg:text-7xl">
+            <h1 className="text-2xl font-bold md:text-4xl lg:text-7xl ">
                 {movie?.title || movie?.name || movie?.original_name}
             </h1>
             <p className="max-w-sx text-xs md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">
